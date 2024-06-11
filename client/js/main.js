794c62610a7853178c6b14ccc27a92adc16b0b8e
oleg.rimko <oleg.rimko@emergingtravel.com>
2024-06-11 15:57:41 +0300

    Copy button + JS formatting

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-(function () {
+;(function () {
   Element.prototype.show = function () {
     this.classList.remove('hidden')
   }
@@ -13,12 +13,16 @@
 
   Element.prototype.fadeOut = function () {
     this.classList.add('fade')
-    setTimeout(() => { this.classList.add('fade-out') }, 1)
-    setTimeout(() => { this.classList.remove('fade-out') }, 1000)
+    setTimeout(() => {
+      this.classList.add('fade-out')
+    }, 1)
+    setTimeout(() => {
+      this.classList.remove('fade-out')
+    }, 1000)
   }
 
-  const restoreDiff = diff => {
-    const lines = diff.split("\n").slice(3)
+  const restoreDiff = (diff) => {
+    const lines = diff.split('\n').slice(3)
     const [fileA, fileB] = [[], []]
     for (const line of lines) {
       const [op, content] = [line[0], line.slice(1)]
@@ -29,17 +33,24 @@
         fileB.push(content)
       }
     }
-    return [fileA.join("\n"), fileB.join("\n")]
+    return [fileA.join('\n'), fileB.join('\n')]
   }
 
-  const diffStats = diff => {
-    const lines = diff.split("\n").slice(3)
-    let added = 0, removed = 0
+  const diffStats = (diff) => {
+    const lines = diff.split('\n').slice(3)
+    let added = 0,
+      removed = 0
     for (const line of lines) {
       if (line[0] === '+') added++
       else if (line[0] === '-') removed++
     }
     return [added, removed]
+  }
+
+  const decode = (data) => {
+    return new TextDecoder().decode(
+      pako.inflate(buffer.Buffer.from(data, 'hex'))
+    )
   }
 
   const $ = document.querySelector.bind(document),
@@ -52,6 +63,7 @@
     const _context = window._diffContext || {}
 
     this.ui = {
+      // loader: $('#loader'),
       controls: $('form#main .controls'),
       compare: $('button.compare'),
       edit: $('button.edit'),
@@ -63,7 +75,7 @@
       diff: $('.diff'),
       modalContent: $('#modal-1-content'),
       modalTitle: $('#modal-1-title'),
-      title: $('input#title')
+      title: $('input#title'),
     }
 
     this.state = {
@@ -106,14 +118,23 @@
     const fileB = this.ui.fileB.value
 
     if (fileA == fileB) {
-      const msg = `Files are ${fileA ? "equal" : "empty"}`
+      const msg = `Files are ${fileA ? 'equal' : 'empty'}`
       throw Error(msg)
     }
     this.state.diff = Diff.createTwoFilesPatch(
-      'fileA', 'fileB', fileA, fileB, 'old', 'new', {
-      context: Infinity
-    })
-    this.state.data = buffer.Buffer(pako.deflate(this.state.diff)).toString('hex')
+      'fileA',
+      'fileB',
+      fileA,
+      fileB,
+      'old',
+      'new',
+      {
+        context: Infinity,
+      }
+    )
+    this.state.data = buffer
+      .Buffer(pako.deflate(this.state.diff))
+      .toString('hex')
     this.state.title = this.ui.title.value
     this.state.created = null
   }
@@ -129,9 +150,13 @@
       synchronisedScroll: true,
       highlight: true,
       renderNothingWhenEmpty: true,
-      stickyHeaders: false
+      stickyHeaders: false,
     }
-    const diff2htmlUi = new Diff2HtmlUI(this.ui.diff, this.state.diff, configuration)
+    const diff2htmlUi = new Diff2HtmlUI(
+      this.ui.diff,
+      this.state.diff,
+      configuration
+    )
     diff2htmlUi.draw()
     diff2htmlUi.highlightCode()
     this.setDiffInfo()
@@ -140,7 +165,7 @@
   App.prototype.setDiffInfo = function () {
     const header = $('.d2h-file-header')
     const title = this.state.title || 'Untitled'
-    // let copyButton = false
+    let copyButton = false
     let html = `<div class="diff-info">`
 
     if (this.state.title) {
@@ -154,29 +179,30 @@
 
     if (this.state.id) {
       const url = this.getUrl()
-      // copyButton = true
+      copyButton = true
       html +=
-        `<div class="url">`
-        + `URL: <a href="${url}">${url}</a>`
-        // + `<span class="copy" data-url="${url}"></span>`
-        // + `<span class="copy-success fade hidden">Copied</span>`
-        + `</div>`
+        `<div class="url">` +
+        `URL: <a href="${url}">${url}</a>` +
+        `<span class="copy" data-url="${url}"></span>` +
+        `<span class="copy-success fade hidden">Copied</span>` +
+        `</div>`
     }
 
     html += `</div>`
 
     const [added, removed] = diffStats(this.state.diff)
-    html += ''
-      + `<div class="diff-stats flex-row">`
-      + `<span class="grow">➖ ${removed} removed</span>`
-      + `<span class="grow">➕ ${added} added</span>`
-      + `</div>`
+    html +=
+      '' +
+      `<div class="diff-stats flex-row">` +
+      `<span class="grow">➖ ${removed} removed</span>` +
+      `<span class="grow">➕ ${added} added</span>` +
+      `</div>`
 
     header.innerHTML = html
 
-    // if (copyButton) {
-    // $('.diff-info .url .copy').onclick = this.copyUrl
-    // }
+    if (copyButton) {
+      $('.diff-info .url .copy').onclick = this.copyUrl
+    }
   }
 
   App.prototype.getUrl = function () {
@@ -185,13 +211,19 @@
 
   App.prototype.copyUrl = async function (e) {
     const target = e.currentTarget
-    const url = target.getAttribute('data-url');
-    await navigator.clipboard.writeText(url)
+    const url = target.getAttribute('data-url')
 
-    // const success = target.nextSibling
-    // success.show()
+    try {
+      await navigator.clipboard.writeText(url)
+    } catch (e) {
+      this.error(e.message)
+      return
+    }
+
+    const success = target.nextSibling
+    success.show()
     // success.fadeOut()
-    // setTimeout(() => success.hide(), 1000)
+    setTimeout(() => success.hide(), 1000)
   }
 
   App.prototype.restore = function () {
@@ -205,6 +237,7 @@
   }
 
   App.prototype.toggleView = function (mode) {
+    // this.ui.loader.hide()
     if (mode == VIEW_MODE_DIFF) {
       this.ui.diff.show()
       this.ui.form.hide()
@@ -237,36 +270,44 @@
 
   App.prototype.save = function () {
     this.ui.save.disabled = true
+    // this.ui.loader.show()
     const body = {
       title: this.state.title,
-      data: this.state.data
+      data: this.state.data,
     }
-    fetch("/api/save", {
+    fetch('/api/save', {
       method: 'POST',
       body: JSON.stringify(body),
-      headers: { 'content-type': 'application/json' }
-    }).then(r => r.json()).then(data => {
-      if (!data.id) {
-        throw Error('Save failed')
-      }
-      this.state.id = data.id
-      this.ui.save.hide()
-      window.location.href = '/' + data.id
-    }).catch(err => {
-      this.error(err)
-    }).finally(() => {
-      this.ui.save.disabled = false
+      headers: { 'content-type': 'application/json' },
     })
+      .then((r) => r.json())
+      .then((data) => {
+        if (!data.id) {
+          throw Error('Save failed')
+        }
+        this.state.id = data.id
+        this.ui.save.hide()
+        window.location.href = '/' + data.id
+      })
+      .catch((err) => {
+        this.error(err)
+      })
+      .finally(() => {
+        this.ui.save.disabled = false
+      })
   }
 
   App.prototype.init = function () {
     MicroModal.init({ debug: true })
     if (this.state.id && this.state.data) {
-      this.state.diff = new TextDecoder().decode(
-        pako.inflate(
-          buffer.Buffer.from(this.state.data, 'hex')
-        )
-      )
+      try {
+        this.state.diff = decode(this.state.data)
+      } catch (e) {
+        console.log(e)
+        this.error('Decode error')
+        this.toggleView(VIEW_MODE_EDIT)
+        return
+      }
       this.render()
       this.toggleView(VIEW_MODE_DIFF)
     } else {
@@ -275,5 +316,7 @@
     return this
   }
 
-  window.onload = function () { window.app = new App().init() }
+  window.onload = function () {
+    window.app = new App().init()
+  }
 })()
